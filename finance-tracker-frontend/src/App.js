694c2d8767a3a4dfc9f425131f4922358e59990c
Xponent123 <143345367+Xponent123@users.noreply.github.com>
2024-12-11 14:03:58 +0530

    Update App.js

--- finance-tracker-frontend/src/App.js
+++ finance-tracker-frontend/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router, Route, and Routes
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Expenses from './components/Dashboard/Expenses';
 import Summary from './components/Dashboard/Summary'; // Import the Summary component
 import './App.css';
-// import './ImageStyles.css'; // Import the new CSS file
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
@@ -13,25 +12,23 @@ function App() {
   return (
     <Router>
       <div className="app-container">
-        <Routes> {/* Use Routes instead of Switch */}
-          <Route path="/summary" element={<Summary token={token} setToken={setToken} />} /> {/* Update Route */}
-          <Route path="/" element={
-            !token ? (
-              <div style={{ display: 'flex', alignItems: 'center', gap: '0px' }}>
-                <div>
-                  <Login setToken={setToken} />
-                  <Register />
-                </div>
-                <img
-                  src="/f1.jpg"
-                  alt="Description"
-                  className="styled-image" // Apply the CSS class
-                />
+        <Routes>
+          <Route path="/" element={!token ? (
+            <div style={{ display: 'flex', alignItems: 'center', gap: '0px' }}>
+              <div>
+                <Login setToken={setToken} />
+                <Register />
               </div>
-            ) : (
-              <Expenses token={token} setToken={setToken} />
-            )
-          } />
+              <img
+                src="/f1.jpg"
+                alt="Description"
+                className="styled-image" // Apply the CSS class
+              />
+            </div>
+          ) : (
+            <Expenses token={token} setToken={setToken} />
+          )} />
+          <Route path="/summary" element={<Summary token={token} />} /> {/* Pass token to Summary */}
         </Routes>
       </div>
     </Router>
